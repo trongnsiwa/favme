67f9dc59b40076caaa631a07211c7ff70a0dbb15
Si Trong <trongnsiwa79@gmail.com>
2022-09-06 08:56:59 +0700

    Add signup page

--- src/pages/sign-up.tsx
+++ src/pages/sign-up.tsx
@@ -2,13 +2,13 @@ import { Button, IconButton, Input, Typography } from "@material-tailwind/react"
 import Link from "next/link";
 import { AiFillEye } from "react-icons/ai";
 
-function LoginPage() {
+function SignUpPage() {
   return (
     <div className="w-full h-screen relative bg-fav-300">
       <div className="w-[35em] min-h-[calc(100vh-500px)] bg-white rounded-2xl shadow-xl absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 px-20 py-10">
         <form>
           <Typography variant="h2" className="flex text-me-50 font-extrabold">
-            Login to <span className="ml-3 text-fav-300">Fav.me</span>
+            Join <span className="mx-3 text-fav-300">Fav.me</span> now!
           </Typography>
           <div className="mt-5">
             <Input
@@ -23,6 +23,7 @@ function LoginPage() {
             <Input
               variant="outlined"
               label="Password"
+              type="password"
               size="lg"
               icon={
                 <IconButton variant="text" className="h-7 w-7" color="light-green">
@@ -33,27 +34,36 @@ function LoginPage() {
               className="text-base"
             />
           </div>
-          <div className="flex justify-end mt-2">
-            <Link href="/forgot-password">
-              <p className="text-sm hover:underline text-fav-500 hover:cursor-pointer">
-                Forgot Password?
-              </p>
-            </Link>
+          <div className="mt-3">
+            <Input
+              variant="outlined"
+              label="Confirm password"
+              type="password"
+              size="lg"
+              icon={
+                <IconButton variant="text" className="h-7 w-7" color="light-green">
+                  <AiFillEye className="h-7 w-7 text-fav-300" />
+                </IconButton>
+              }
+              color="light-green"
+              className="text-base"
+            />
           </div>
+
           <div className="divide-y divide-gray-300 w-full">
             <Button type="button" className="btn btn-fav mt-5 shadow-fav">
-              Sign In
+              Sign Up
             </Button>
             <div className="text-gray-400 w-full text-center mt-10 pt-3">or</div>
             <Button type="button" className="btn btn-git mt-3 shadow-fav">
-              Sign-in with Github
+              Sign-up with Github
             </Button>
           </div>
         </form>
         <div className="flex justify-center align-center mt-10">
-          <p className="pr-2 text-sm">Don&apos;t have any account? </p>
-          <Link href="/sign-up">
-            <p className="text-fav-500 text-sm hover:underline hover:cursor-pointer">Register</p>
+          <p className="pr-2 text-sm">Already have account? </p>
+          <Link href="/login">
+            <p className="text-fav-500 text-sm hover:underline hover:cursor-pointer">Login</p>
           </Link>
         </div>
       </div>
@@ -61,4 +71,4 @@ function LoginPage() {
   );
 }
 
-export default LoginPage;
+export default SignUpPage;
