ba0d02be134ede4f1147c5f455e41931e1f98b8d
Si Trong <trongnsiwa79@gmail.com>
2022-09-14 12:39:47 +0700

    Return image optimization

--- next.config.mjs
+++ next.config.mjs
@@ -15,7 +15,6 @@ function defineNextConfig(config) {
 export default defineNextConfig({
   reactStrictMode: true,
   images: {
-    path: "/",
     domains: ["lh3.googleusercontent.com", "https://vectorified.com/images"]
   }
 });
